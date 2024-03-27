66ce4998d33d8a27807ad3739d4c96c8fcb10a8a
gou-bei <77963078+gou-bei@users.noreply.github.com>
2024-03-27 16:30:35 +0800

    finish

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,7 @@ import time
 import pymysql
 pymysql.install_as_MySQLdb()
 app = Flask(__name__)
-time.sleep(5)
+time.sleep(10)
 db = pymysql.connect(host="db",user="web",password="P@ssw0rd",database="hdxw",charset='utf8mb4')
 
 cursor = db.cursor()
